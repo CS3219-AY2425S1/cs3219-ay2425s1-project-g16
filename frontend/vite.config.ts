bbdac6698ed64b966e3b26a6259230ad6c98f8f1
SeeuSim <seeusimong@gmail.com>
2024-10-17 13:23:48 +0800

    feat/ci: test dyn matrix
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// feat-ci: test
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
