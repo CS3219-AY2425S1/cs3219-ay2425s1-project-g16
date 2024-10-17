13711b6b11be57b5c5cddbc8e6e2914ca3e49d85
SeeuSim <seeusimong@gmail.com>
2024-10-17 13:27:16 +0800

    feat/ci: push final config
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,6 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// feat-ci: test
-// feat-ci: test
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
