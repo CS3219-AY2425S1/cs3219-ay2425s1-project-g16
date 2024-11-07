39fb9dc98f90b96f7ad75f7fb13b281627e93c4f
SeeuSim <seeusimong@gmail.com>
2024-11-07 17:42:54 +0800

    try fix build bug
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- frontend/src/components/blocks/questions/admin-edit-form.tsx
+++ frontend/src/components/blocks/questions/admin-edit-form.tsx
@@ -1,1 +1,23 @@
-export const AdminEditForm = () => {};
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+export const AdminEditForm = () => {
+  return (
+    <Dialog>
+      <DialogTrigger />
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle />
+        </DialogHeader>
+        <DialogContent />
+        <DialogFooter />
+      </DialogContent>
+    </Dialog>
+  );
+};
